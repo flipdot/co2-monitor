e3ed78926d0e120cf31fd9553f4985f47264146b
nox_x <nox-x@users.noreply.github.com>
2017-05-26 20:33:23 +0200

    Clean up code, add exec-options, reduce memory usage

--- monitor.c
+++ monitor.c
@@ -6,6 +6,9 @@
 
 #include <linux/hidraw.h>
 #include <linux/input.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdint.h>
 /*
  * Ugly hack to work around failing compilation on systems that don't
   * yet populate new version of hidraw.h to userspace.
@@ -21,212 +24,249 @@
 #define PID "a052"
 #define NODEV "[NODEV]"
 
-struct data_struct {
-	double temp;
-	int co2_value;
+struct value_struct {
+    double temp;
+    uint16_t co2;
+    char state;
 };
+int flag = -1;
 
 char *get_path();
 
-int generate_out(struct data_struct *data);
+int generate_out(struct value_struct *data);
 
-int decrypt(int key[8], int *data, int out[8]);
+uint64_t pseudo_decrypt(uint64_t *in_data);
 
-int main(void)
-{
-	printf("\033[1;34m[INFO]\033[m starting the monitor.\n");
+void print_help();
 
-	char *path;
-	int file_descriptor, result;
-	char hid_io_feature[9] =
-			{(char) 0x00, (char) 0xc4, (char) 0xc6,
-			 (char) 0xc0, (char) 0x92, (char) 0x40,
-			 (char) 0x23, (char) 0xd, (char) 0x96};
+int main(const int argc, char **argv) {
 
+    int c;
+    while ((c = getopt(argc, argv, "ld:h")) != -1) {
+        switch (c) {
+            case 'h':
+                print_help();
+                return 0;
+            case 'l':
+                flag = 1;
+                break;
+            case 'd': {
+                // TODO implement debug levels
+                int op = atoi(optarg);
+                if (op >= 0 && op <= 3) {
+                    flag = op + 3;
+                } else {
+                    print_help();
+                    return 1;
+                }
+                break;
+            }
+            default:
+                print_help();
+                return 1;
+        }
+    }
 
-	/* searching for device */
-	path = get_path();
-	if (strcmp(path, NODEV) == 0)
-	{
-		perror("\033[1;31m[ERROR]\033[m can't find device");
-		return 1;
-	}
-	printf("\033[1;34m[INFO]\033[m found dev on path %s.\n", path);
+    if (flag != 1)
+        printf("\033[1;34m[INFO]\033[m starting the monitor.\n");
 
-	/* open device */
-	file_descriptor = open(path, O_RDWR);
-	if (file_descriptor < 0)
-	{
-		perror("\033[1;31m[ERROR]\033[m Unable to open device");
-		return 1;
-	}
+    char *path;
+    int file_descriptor, result;
+    uint8_t hid_io_feature[9] = {0};
 
-	printf("\033[1;34m[INFO]\033[m opened device %s successfully.\n", path);
+/* searching for device */
+    path = get_path();
+    if (strcmp(path, NODEV) == 0) {
+        if (flag != 1)
+            perror("\033[1;31m[ERROR]\033[m can't find device\nExiting...");
+        else
+            perror("[ERROR] can't find device\nExiting...");
+        return 2;
+    }
+    if (flag != 1)
+        printf("\033[1;34m[INFO]\033[m found dev on path %s.\n", path);
 
-	/* sending feature 'SET_REPORT' */
-	result = ioctl(file_descriptor, HIDIOCSFEATURE(9), hid_io_feature);
-	if (result < 0)
-		perror("HIDIOCSFEATURE");
+/* open device */
+    file_descriptor = open(path, O_RDWR);
+    if (file_descriptor < 0) {
+        if (flag != 1)
+            perror("\033[1;31m[ERROR]\033[m Unable to open device\nExiting...");
+        else
+            perror("[ERROR] Unable to open device\nExiting...");
+        return 3;
+    }
 
+    if (flag != 1)
+        printf("\033[1;34m[INFO]\033[m opened device %s successfully.\n", path);
 
-	/* reading from device */
-	int encrypted[8] = {0};
-	int decrypted[8] = {0};
-	char tmp[8] = {0};
-	int key[8] = {0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96};
-	struct data_struct data;
+/* sending feature 'SET_REPORT' */
+    result = ioctl(file_descriptor, HIDIOCSFEATURE(9), hid_io_feature);
+    if (result < 0) perror("HIDIOCSFEATURE");
 
-	for (int i = 0; i < 11; i++)
-	{
-		result = (int) read(file_descriptor, tmp, 16);
+/* reading from device */
+    uint64_t recieved_data = 0;
+    struct value_struct data = {0, 0, 0};
 
-		for (int k = 0; k < 8; k++)
-			encrypted[k] = tmp[k] & 0xFF;
+    do {
+        result = (int) read(file_descriptor, &recieved_data, 8);
 
+        if (result < 0) {
+            if (flag != 1)
+                perror("\033[1;31m[ERROR]\033[m reading from device\nExiting...");
+            else
+                perror("[ERROR] reading from device\nExiting...");
+        } else {
+/* decrypt read bytes */
+            pseudo_decrypt(&recieved_data);
 
-		if (result < 0)
-		{
-			perror("\033[1;31m[ERROR]\033[m reading from device ");
-		} else
-		{
-			/* decrypt read bytes */
-			decrypt(key, encrypted, decrypted);
+/* get data from encryption */
+            if ((recieved_data >> 56) == 0x50) {
+                data.co2 = (uint16_t)(recieved_data >> 40);
+                data.state |= 0x1;
+            } else if ((recieved_data >> 56) == 0x42) {
+                data.temp = ((recieved_data >> 40) & 0xffff) / 16.0 - 273.15;
+                data.state |= 0x2;
+            }
+        }
+    } while (data.state != 3);
 
-			/* get data from encryption */
-			if(decrypted[0] == 0x50)
-			{
-				data.co2_value = decrypted[1] << 8 | decrypted[2];
-			} else if (decrypted[0] == 0x42)
-			{
-				data.temp = (decrypted[1] << 8 | decrypted[2]) / 16.0 - 273.15;
-			}
-		}
-	}
+/* generating output */
+    generate_out(&data);
 
-	/* generating output */
-	generate_out(&data);
+/* closing device */
+    close(file_descriptor);
 
-	/* closing device */
-	close(file_descriptor);
-	printf("\033[1;34m[INFO]\033[m closed device %s successfully.\n", path);
-	return 0;
+    if (flag != 1)
+        printf("\033[1;34m[INFO]\033[m closed device %s successfully.\n", path);
+
+    return 0;
 }
 
-char *get_path()
-{
-	const char *out;
-	struct udev *udev;
-	struct udev_enumerate *enumerate;
-	struct udev_list_entry *devices, *dev_list_entry;
-	struct udev_device *dev;
-	const char *path, *vid, *pid;
+char *get_path() {
+    const char *out;
+    struct udev *udev;
+    struct udev_enumerate *enumerate;
+    struct udev_list_entry *devices, *dev_list_entry;
+    struct udev_device *dev;
+    const char *path, *vid, *pid;
 
-	/* creating udev-device */
-	udev = udev_new();
+    /* creating udev-device */
+    udev = udev_new();
 
-	if (!udev)
-	{
-		printf("\033[1;31m[ERROR]\033[m can't create udev device\n");
-		return NODEV;
-	}
+    if (!udev) {
+        printf("\033[1;31m[ERROR]\033[m can't create udev device\n");
+        return NODEV;
+    }
 
-	/* create a list of the devices in the 'hidraw' subsystem. */
-	enumerate = udev_enumerate_new(udev);
-	udev_enumerate_add_match_subsystem(enumerate, HIDRAW);
-	udev_enumerate_scan_devices(enumerate);
-	devices = udev_enumerate_get_list_entry(enumerate);
+    /* create a list of the devices in the 'hidraw' subsystem. */
+    enumerate = udev_enumerate_new(udev);
+    udev_enumerate_add_match_subsystem(enumerate, HIDRAW);
+    udev_enumerate_scan_devices(enumerate);
+    devices = udev_enumerate_get_list_entry(enumerate);
 
-	udev_list_entry_foreach(dev_list_entry, devices)
-	{
+    udev_list_entry_foreach(dev_list_entry, devices) {
 
 
-		/* Get the filename of the /sys entry for the device
-		   and create a udev_device object (dev) representing it */
-		path = udev_list_entry_get_name(dev_list_entry);
-		dev = udev_device_new_from_syspath(udev, path);
+        /* Get the filename of the /sys entry for the device
+           and create a udev_device object (dev) representing it */
+        path = udev_list_entry_get_name(dev_list_entry);
+        dev = udev_device_new_from_syspath(udev, path);
 
-		/* usb_device_get_devnode() returns the path to the device node
-		   itself in /dev. */
-		out = udev_device_get_devnode(dev);
+        /* usb_device_get_devnode() returns the path to the device node
+           itself in /dev. */
+        out = udev_device_get_devnode(dev);
 
 
-		/* The device pointed to by dev contains information about
-		   the hidraw device. In order to get information about the
-		   USB device, get the parent device with the
-		   subsystem/devtype pair of "usb"/"usb_device". This will
-		   be several levels up the tree, but the function will find
-		   it.*/
-		dev = udev_device_get_parent_with_subsystem_devtype(
-				dev,
-				"usb",
-				"usb_device");
-		if (!dev)
-		{
-			printf("\033[1;31m[ERROR]\033[m Unable to find parent usb device.");
-			return NODEV;
-		}
+        /* The device pointed to by dev contains information about
+           the hidraw device. In order to get information about the
+           USB device, get the parent device with the
+           subsystem/devtype pair of "usb"/"usb_device". This will
+           be several levels up the tree, but the function will find
+           it.*/
+        dev = udev_device_get_parent_with_subsystem_devtype(
+                dev,
+                "usb",
+                "usb_device");
+        if (!dev) {
+            printf("\033[1;31m[ERROR]\033[m Unable to find parent usb device.");
+            return NODEV;
+        }
 
-		vid = udev_device_get_sysattr_value(dev, "idVendor");
-		pid = udev_device_get_sysattr_value(dev, "idProduct");
-		int proof = strcmp(vid, VID) == 0 && strcmp(pid, PID) == 0;
+        vid = udev_device_get_sysattr_value(dev, "idVendor");
+        pid = udev_device_get_sysattr_value(dev, "idProduct");
+        int proof = strcmp(vid, VID) == 0 && strcmp(pid, PID) == 0;
 
-		udev_device_unref(dev);
+        udev_device_unref(dev);
 
-		/* return the path of the device if the pid and vid are correct */
-		if (proof)
-		{
-			return (char *) out;
-		}
+        /* return the path of the device if the pid and vid are correct */
+        if (proof) {
+            return (char *) out;
+        }
 
-	}
+    }
 
-	return NODEV;
+    return NODEV;
 }
 
-int decrypt(int key[8], int *data, int out[8])
-{
-	int cstate[8] = {0x48, 0x74, 0x65, 0x6D, 0x70, 0x39, 0x39, 0x65};
-	int shuffel[8] = {2, 4, 0, 7, 1, 6, 5, 3};
+uint64_t pseudo_decrypt(uint64_t *in_data) {
+    uint64_t out = 0;
 
-	/* phase 1 */
-	int phase1[8] = {0};
-	for (int d = 0; d < 8; d++)
-		phase1[shuffel[d]] = data[d];
+    /* phase 1 */
+    uint8_t shuffle[8] = {2, 4, 0, 7, 1, 6, 5, 3};
 
-	/* phase 2 */
-	int phase2[8] = {0};
-	for (int d = 0; d < 8; d++)
-		phase2[d] = phase1[d] ^ key[d];
+    for (int d = 0; d < 8; ++d) {
+        out |= ((*in_data >> (d * 8)) & 0xff) << (shuffle[d] * 8);
+    }
+    *in_data = 0;
 
-	/* phase 3 */
-	int phase3[8] = {0};
-	for (int d = 0; d < 8; d++)
-		phase3[d] = ((phase2[d] >> 3) | (phase2[(d - 1 + 8) % 8] << 5)) & 0xff;
+    uint64_t out_tmp = out;
+    out = 0;
 
-	/* phase 4 */
+    /* phase 2 is obsolete */
 
-	int ctmp[8] = {0};
-	for (int d = 0; d < 8; d++)
-		ctmp[d] = ((cstate[d] >> 4) | (cstate[d] << 4)) & 0xff;
+    /* swapping out */
+    for (int j = 0; j < 8; ++j) {
+        out |= ((out_tmp >> (j * 8)) & 0xff) << ((7 - j) * 8);
+    }
 
-	for (int d = 0; d < 8; d++)
-	{
-		out[d] = (0x100 + phase3[d] - ctmp[d]) & 0xff;
-	}
 
-	return 0;
+    /* phase 3 */
+    uint64_t mask = out << 61;
+    out = (out >> 3) | mask;
+
+    /* phase 4 */
+    uint8_t cstate[8] = {0x84, 0x47, 0x56, 0xD6, 0x07, 0x93, 0x93, 0x56};
+    mask = (uint64_t) 0xFF << 56;
+
+    for (int i = 0; i < 8; ++i) {
+        uint8_t buffer = (uint8_t) ((((out & mask) >> 56) - cstate[i]) & 0xff);
+        out <<= 8;
+        out |= buffer;
+    }
+
+    *in_data = out;
+    return 0;
 }
 
-int generate_out(struct data_struct *data)
-{
-	/* TODO dealing with the data in the data_struct data
-	 * i.e.
-	 * 	(i)		updating space-api
-	 * 	(ii)	save in a file
-	 * 	(iii)	blink some lights
-	 * 	(iv)	create a timestamp
-	 */
-	printf("\033[1;33m[VALUE]\033[m CO2: %i Temp: %.2lf\n", data -> co2_value, data -> temp);
+int generate_out(struct value_struct *data) {
+    /* TODO dealing with the data in the data_struct data
+     * i.e.
+     * 	(i)		updating space-api
+     * 	(ii)	save in a file
+     * 	(iii)	blink some lights
+     */
 
-	return 0;
+    if (flag != 1)
+        printf("\033[1;33m[VALUE]\033[m CO2: %i Temp: %.2lf\n", data->co2, data->temp);
+    else
+        printf("%i\n%.2lf\n", data->co2, data->temp);
+
+    return 0;
+}
+
+void print_help() {
+    printf("co2monitor [options]\n"
+                   "    --help      -h                 display this help\n"
+                   "    --headless  -l                 run in headless-mode reduces output to an minimum\n"
+                   "    --debug     -d <debug-level>   higher debuglevel increases output (range: 0-3)\n");
 }
