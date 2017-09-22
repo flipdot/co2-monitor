5b9af91961bc9adcee7195a16e219ddc03cb8b4f
Felix Möller <git@felix-moeller.net>
2017-09-22 19:25:07 +0200

    Add co2 mqtt polling daemon

---
+++ co2d.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# CO2-Monitor Daemon
+#
+# Polls CO2 values by invoking 'monitor -l' periodically and publishes them to
+# a specific MQTT topic.
+#
+# TODO
+# - Use config file to store constants
+
+import json
+import subprocess
+import time
+import paho.mqtt.client as mqtt
+
+MQTT_HOST = 'localhost'
+MQTT_TOPIC_CO2 = 'sensors/lounge/co2'
+POLL_INTERVAL = 5  # seconds
+MONITOR_BIN = '/home/co2-monitor/co2-monitor/monitor'
+MONITOR_ARGS = ['-l']
+
+
+def get_co2():
+    call = [MONITOR_BIN] + MONITOR_ARGS
+    proc = subprocess.Popen(call, stdout=subprocess.PIPE)
+    ret = proc.stdout.readline()
+    co2 = ret.split()[1]
+    return int(co2)
+
+
+client = mqtt.Client()
+client.connect(MQTT_HOST)
+
+while True:
+    # Create JSON object containing CO2 value
+    json_data = {}
+    json_data['co2'] = get_co2()
+    json_text = json.dumps(json_data)
+
+    # Publish the value to the corresponding topic on the broker
+    client.publish(MQTT_TOPIC_CO2, json_text)
+
+    # Wait for the configured interval
+    time.sleep(POLL_INTERVAL)
